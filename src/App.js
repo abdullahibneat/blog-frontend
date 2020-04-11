14334874a0ca4c0e052dd4cc32208f1f4c94a077
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-11 13:17:23 +0100

    Part 5.2: Clear localStorage on logout, keep user logged in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(JSON.parse(window.localStorage.getItem("user")))
 
     useEffect(() => {
         blogService.getAll().then(blogs => setBlogs(blogs))
@@ -36,7 +36,7 @@ const App = () => {
     const blogsContainer = () => (
         <div>
             <h2>blogs</h2>
-            <p>Hi {user.name}! <button onClick={event => setUser(null)}>logout</button></p>
+            <p>Hi {user.name}! <button onClick={event => { setUser(null); window.localStorage.clear() }}>logout</button></p>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog} />
             )}
