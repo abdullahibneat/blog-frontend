e51e52f1e4888390994903332e87a4d2db59db56
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-11 12:57:32 +0100

    Part 5.1: Allow user login, display blogs after successful authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
+import loginService from "./services/login"
+
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState(null)
 
     useEffect(() => {
         blogService.getAll().then(blogs => setBlogs(blogs))
     }, [])
 
-    return (
+    const handleLogin = async event => {
+        event.preventDefault()
+        try {
+            setUser(await loginService.login({ username, password }))
+        } catch(err) {
+            alert("Wrong credentials")
+        }
+    }
+
+    const loginForm = () => (
+        <form onSubmit={handleLogin}>
+            <div>Username: <input type="text" value={username} name="username" onChange={ ({ target }) => setUsername(target.value) } /></div>
+            <div>Password: <input type="password" value={password} name="password" onChange={ ({ target }) => setPassword(target.value) } /></div>
+            <button type="submit">Login</button>
+        </form>
+    )
+
+    const blogsContainer = () => (
         <div>
             <h2>blogs</h2>
             {blogs.map(blog =>
@@ -17,6 +39,8 @@ const App = () => {
             )}
         </div>
     )
+
+    return (user === null? loginForm() : blogsContainer())
 }
 
 export default App
