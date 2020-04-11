28a7c4636f00a1f6efbf4ce47b294073f29fb688
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-11 12:19:10 +0100

    Add eslint config, fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	"env": {
+		"browser": true
+	},
+	"extends": "eslint:recommended",
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module",
+		"allowImportExportEverywhere": true
+	},
+	"rules": {
+		"indent": [
+			"error",
+			4
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"eqeqeq": "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": [
+			"error", "always"
+		],
+		"arrow-spacing": [
+			"error", { "before" : true, "after" : true }
+		],
+		"no-console": 0,
+		"no-unused-vars": [
+			"warn"
+		]
+	}
+}
