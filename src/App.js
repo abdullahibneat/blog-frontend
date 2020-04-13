8553f4ff5f4e0e90f7b332d7122701ff02ad6bde
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-13 20:25:57 +0100

    Part 5.9: Sort blogs by number of likes

--- src/App.js
+++ src/App.js
@@ -71,9 +71,7 @@ const App = () => {
                 {addBlogForm()}
             </div>
         }
-        {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-        )}
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog => <Blog key={blog.id} blog={blog} />)}
     </>)
 }
 
