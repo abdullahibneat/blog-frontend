b55741fe5dc504ca7c8953b211d3b51ad2168d5a
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-13 15:06:29 +0100

    Part 5.7: Allow blog details to be expanded/collapsed

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,20 @@
 import React from "react"
+import Toggable from "./Toggable"
+
+const blogStyle = {
+    border: "solid",
+    borderWidth: 1,
+    margin: "1rem",
+    padding: "0.5rem"
+}
 
 const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
+    <div style={blogStyle}>
+        {blog.title} <Toggable buttonLabel="view">
+            <p>{blog.URL}</p>
+            <p>{blog.likes} <button>like</button></p>
+            <p>{blog.author}</p>
+        </Toggable>
     </div>
 )
 
