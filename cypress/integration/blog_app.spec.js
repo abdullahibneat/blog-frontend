46244c05ef11b08d40c9b2d6228c4210a5ada6a4
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-18 11:48:12 +0100

    Create a test user after emptying db

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -2,7 +2,12 @@
 
 describe("Blog app", () => {
     beforeEach(() => {
-        cy.request("POST", "http://localhost:3002/api/testing/reset")
+        cy.request("POST", "http://localhost:3002/api/testing/reset") // Reset database
+        cy.request("POST", "http://localhost:3002/api/users", { //Create test user
+            name: "test",
+            username: "user",
+            password: "pass"
+        })
         cy.visit("http://localhost:3000")
     })
 
