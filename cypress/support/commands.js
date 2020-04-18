56ba6c0d7eba1013e2fd6a7dc6eda286bc754842
Abdullah Ibne Atiq <hello@abdu.io>
2020-04-18 12:18:02 +0100

    Create a cypress command to add a new blog

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,13 @@ Cypress.Commands.add("login", ({ username, password }) => {
             cy.visit("http://localhost:3000")
         })
 })
+
+Cypress.Commands.add("newBlog", ({ title, author, url }) => {
+    cy.request({
+        url: "http://localhost:3002/api/blogs",
+        method: "POST",
+        body: { title, author, url },
+        headers: { "Authorization": JSON.parse(localStorage.getItem("user")).token }
+    })
+    cy.visit("http://localhost:3000")
+})
