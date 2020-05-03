cf8648e20065d225d8bf4d57de8bdcae77206470
Abdullah Ibne Atiq <hello@abdu.io>
2020-05-03 12:53:17 +0100

    Remove all styling and Toggable from blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,16 +1,8 @@
 import React from "react"
-import Toggable from "./Toggable"
 import { useSelector, useDispatch } from "react-redux"
 import { deleteBlog, like } from "../reducers/blogReducer"
 import { setNotification } from "../reducers/notificationReducer"
 import { useParams } from "react-router-dom"
-
-const blogStyle = {
-    border: "solid",
-    borderWidth: 1,
-    margin: "1rem",
-    padding: "0.5rem"
-}
 
 const Blog = () => {
     const id = useParams().id
@@ -34,14 +26,13 @@ const Blog = () => {
     }
 
     return blog
-        ? <div className="blog" style={blogStyle}>
+        ? <div className="blog">
             <h4 className="blogTitle">{blog.title}</h4>
-            <p>by <span className="blogAuthor">{blog.author}</span></p><Toggable buttonLabel="view">
+            <p>by <span className="blogAuthor">{blog.author}</span></p>
                 <p className="blogURL">{blog.url}</p>
                 <p>likes <span className="blogLikes">{blog.likes}</span> <button id="blogLikeButton" onClick={handleLike}>like</button></p>
                 <p>Saved by {blog.user.name}</p>
                 {username === blog.user.name ? <button onClick={handleDelete}>delete</button> : null}
-            </Toggable>
         </div>
         : <h4>Blog not found</h4>
 }
