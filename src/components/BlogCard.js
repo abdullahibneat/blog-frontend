d5384d3dc6290aa80ff307100b625254e7884d50
Abdullah Ibne Atiq <hello@abdu.io>
2020-05-07 19:51:54 +0100

    Add prop-types for BlogCard

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { Card, AnchorButton } from "@blueprintjs/core"
+import PropTypes from "prop-types"
 
 const BlogCard = ({ blog }) => <Card className="blogCard" key={blog.id}>
     <h2 style={{ marginTop: 0 }}>{blog.title}</h2>
     <p>by {blog.author}</p>
     <AnchorButton text="Read More" href={`/blogs/${blog.id}`} />
 </Card>
+
+BlogCard.propTypes = {
+    blog: PropTypes.object.isRequired
+}
 
 export default BlogCard
