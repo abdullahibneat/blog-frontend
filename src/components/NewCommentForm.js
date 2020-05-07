e8ac21b21bb5817ce38615232e768f07e151b801
Abdullah Ibne Atiq <hello@abdu.io>
2020-05-07 21:34:55 +0100

    Temporarily disable form until comment is added

--- src/components/NewCommentForm.js
+++ src/components/NewCommentForm.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useParams } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import blogService from "../services/blogs"
@@ -7,24 +7,27 @@ import { setNotification } from "../reducers/notificationReducer"
 import { FormGroup, Button, TextArea } from "@blueprintjs/core"
 
 const NewCommentForm = () => {
+    const [disabled, setDisabled] = useState(false)
     const id = useParams().id
     const dispatch = useDispatch()
 
     const handleSubmit = event => {
         event.preventDefault()
         event.persist()
+        setDisabled(true)
         blogService.comment(id, event.target.comment.value)
             .then(blog => dispatch(updateBlog(blog)))
             .then(_ => event.target.reset())
             .catch(err => dispatch(setNotification("Comment cannot be empty", "ERROR")))
+            .finally(() => setDisabled(false))
     }
 
     return <div>
         <form onSubmit={handleSubmit}>
             <FormGroup labelFor="comment">
-                <TextArea id="comment" name="comment" placeholder="Leave a comment. E.g. Thanks for sharing this article!" />
+                <TextArea disabled={disabled} id="comment" name="comment" placeholder="Leave a comment. E.g. Thanks for sharing this article!" />
             </FormGroup>
-            <Button icon="chat" text="Comment" type="submit" />
+            <Button loading={disabled} icon="chat" text="Comment" type="submit" />
         </form>
     </div>
 }
